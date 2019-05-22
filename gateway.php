8d9e28349dd45946951cf86788c793952b0bfe1b
Jyles Coad-Ward <jylescoadward@gmail.com>
2019-05-22 17:00:07 +0800

    yay a chatroom!

--- gateway.php
+++ gateway.php
@@ -1,27 +1,60 @@
 <?php
-	$page = "false";
-	if($page === "home"){
+	//$page = "false";
+
+	$ipAddress = $_SERVER['REMOTE_ADDR'];
+    date_default_timezone_set('Australia/Perth');
+	$currentDate = date('m/d/Y h:i:s a', time());
+	echo($page);
+
+	function writeLog($note){
+        $file = fopen("log.txt","a");
+
+        //Write Useful Information to the Log
+        fwrite("Date: ".currentDate."<br>");
+        fwrite("IP Address: ".ipAddress."<br>");
+        fwrite("Destination: ".page."<br>");
+        fwrite("Note: ".note."<br><br><br>");
+
+        fclose($file);
+	}
+
+	if($page = "home" || $page = "index") {
 		header("Location: http://computerclub/");
-	}
-	elseif ($id === "registration") {
-		header("Location: https://goo.gl/forms/WNHCHnam4IDz6Jzq1");
-	}
-	elseif ($id === "teams"){
-		header("Location: https://docs.google.com/document/d/1GNM9Qwg0KFl9mBl5hAtTkj7iPB5WhQRivPq5NiJNw7c/view");
-	}
-	elseif (id === "challenges"){
-		header("Location: http://shenton_au.libguides.com/clubs/computerclub");
-	}
-	elseif ($page === "chatroom"){
-		header("Location: http://computerlcub/chat");
-	}
-	elseif ($page === "php-collab"){
-		header("Location: http://computerclub/~coderdojo/colab");
-	}
-	elseif ($page === "angus"){
-		header("Locaiton: http://computerclub/~coderdojo");
-	}
-	else{
-		header('Location: ' . $_SERVER['HTTP_REFERER']);
-	}
+		echo("home or index");
+	} else{
+	    if ($page = "registration") {
+            header("Location: https://goo.gl/forms/WNHCHnam4IDz6Jzq1");
+            echo "register";
+        } else {
+	        if ($page = "teams") {
+                header("Location: https://docs.google.com/document/d/1GNM9Qwg0KFl9mBl5hAtTkj7iPB5WhQRivPq5NiJNw7c/view");
+                echo "teams";
+            } else{
+	            if ($page = "challenges") {
+                    header("Location: http://shenton_au.libguides.com/clubs/computerclub");
+                    echo "challenges";
+                } else{
+	                if ($page = "chat") {
+                        header("Location: http://computerlcub/chat");
+                        echo "chat";
+                    } else{
+	                    if ($page = "collab") {
+                            header("Location: http://computerclub/~coderdojo/colab");
+                            echo "collab";
+                        } else{
+	                        if ($page = "angus") {
+                                header("Locaiton: http://computerclub/~coderdojo");
+                                echo "Coder Dojo";
+                            } else{
+	                            if ($page !== "angus" || "collab" || "chat" || "challenges" || "teams" | "registration" || "index" || "home") {
+                                    die('Forbidden');
+                                    echo "Forbidden";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 ?>
